1ff1ea139dcbcc381d10f667e6276aca40e001e8
LevyAM <levyzudo@msn.com>
2021-02-01 21:42:58 -0300

    Feito o front com partials

--- 06.projeto-perguntas_respostas/index.js
+++ 06.projeto-perguntas_respostas/index.js
@@ -5,29 +5,13 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
-app.get("/:nome/:lang", (req, res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = false;
-
-    var produtos = [
-        {nome: "Xilito", preco: 0.75},
-        {nome: "Suco", preco: 0.75},
-        {nome: "Dindin", preco: 2},
-        {nome: "Saguadim", preco: 1},
-        {nome: "Babalu", preco: 0.25},
-        {nome: "Toddy", preco: 0.80}
-        
-    ]
-
-    res.render("index.ejs", {
-        nome: nome,
-        lang: lang,
-        empresa: "NoDevelopers",
-        funcionarios: 1,
-        msg: exibirMsg,
-        produtos: produtos
-    });
+app.get("/", (req, res) => {
+       res.render("index.ejs");
 });
+
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar.ejs");
+})
+
 
 app.listen(8080, ()=>{console.log("App rodando");});
