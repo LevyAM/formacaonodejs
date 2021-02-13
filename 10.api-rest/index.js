3cb72f93e8936b99ece0bf86234423d17e7b8f0e
LevyAM <levyzudo@msn.com>
2021-02-12 22:51:39 -0300

    Fazendo a parte de usuário e autenticação

--- 10.api-rest/index.js
+++ 10.api-rest/index.js
@@ -1,16 +1,20 @@
 const express = require("express")
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
+
 
 dotenv.config({path: "./config/config.env"});
 
-const app = express();
 
+
+const app = express();
+app.use(cors());
+const jwtSecret = process.env.MY_SECRET;
 const PORT = process.env.MY_PORT;
 
-console.log(`Hostname:${process.env.BD_HOSTNAME}`)
-
-
+const User = require("./database/User")
 const Game = require("./database/Game");
 const connection = require("./database/database.js");
 
@@ -25,6 +29,82 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
+function auth(req,res,next) {
+
+	const authToken = req.headers.authorization;
+	//A FAZER
+
+}
+
+
+
+
+
+
+app.post("/auth", async (req, res) => {
+	let {email, password} = req.body;
+
+	if(email != undefined){
+		let user = await User.findOne({where: {email: email}});
+		
+		try {
+			if(user.password == password){
+				jwt.sign({id: user.id, email: user.email}, jwtSecret, {expiresIn:'24h'}, (error, token) => {
+					if(error){
+						res.status(404);
+						console.log("sem token")
+					} else {
+						res.status(200);
+						res.json({token: token});
+					}
+				})
+				res.status(200);
+			} else {
+				res.sendStatus(401);
+				console.log("senha incorreta")
+			}
+
+		} catch (error) {
+			res.sendStatus(404);
+		}
+
+	} else {
+		res.sendStatus(400);
+		console.log("email errado")
+	}
+})
+
+
+
+
+app.get("/users", async (req, res) => {
+	try {
+		let allUsers = await User.findAll({raw:true, order: [['id', 'ASC']]});
+		allUsers ? res.status(200).json(allUsers) : res.sendStatus(204);
+	} catch (error) {
+		res.sendStatus(400);
+		console.log(error);
+	}
+})
+
+
+
+
+app.post("/user", async (req, res) => {
+	let {name, email, password} = req.body;
+	try {
+		await User.create({
+			name: name,
+			email: email,
+			password: password
+		});
+  		res.sendStatus(201);
+	} catch (error) {
+		console.log(error)
+		res.sendStatus(400);
+	}
+});
+
 
 app.get("/games", async (req, res) => {
 
@@ -38,6 +118,7 @@ app.get("/games", async (req, res) => {
 	}
 	
 })
+
 
 app.get("/games/:id", async (req, res) => {
 	
